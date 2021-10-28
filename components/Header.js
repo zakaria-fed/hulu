a518bbee154744bc684b4d936bcb615114b3d2eb
zakaria-fed <zakariaaskary15@gmail.com>
2021-10-28 18:37:10 +0100

    Some Changes

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <header className="">
+      <Image src="https://links.papareact.com/ua6" height={100} />
+      {/* Reusable Icons */}I am Reusable
+      {/* Logo */}
+    </header>
+  );
+};
+
+export default Header;
